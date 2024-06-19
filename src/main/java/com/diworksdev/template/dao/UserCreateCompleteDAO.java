034115c377dfd8aeef1245c439af5861ca4104e3
Kana-Chris <nyapo797@gmail.com>
2024-06-19 21:32:23 +0900

    UserCreateComplete

--- src/main/java/com/diworksdev/template/dao/UserCreateCompleteDAO.java
+++ src/main/java/com/diworksdev/template/dao/UserCreateCompleteDAO.java
@@ -1,4 +1,4 @@
-package com.diworksdev.template.action;
+package com.diworksdev.template.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -8,7 +8,7 @@ import com.diworksdev.template.util.DateUtil;
 
 public class UserCreateCompleteDAO {
 	private DateUtil dateUtil = new DateUtil();
-	private String sql = "INSERT INTO login_user_transaction(login_id,login_pass,user_name,insert_date) VALUES(?,?,?)";
+	private String sql = "INSERT INTO login_user_transaction(login_id,login_pass,user_name,insert_date) VALUES(?,?,?,?)";
 	
 	public void createUser(String loginUserId,String loginUserPassword,String userName) throws SQLException{
 		DBConnector dbConnector = new DBConnector();
